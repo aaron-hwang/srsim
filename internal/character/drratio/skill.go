ff9a39be9b07ce26d4c5fd9b1eb552b6ee139b6b
ducc <aaronhwang22@gmail.com>
2025-01-15 18:00:11 -0500

    skill skeleton

--- internal/character/drratio/skill.go
+++ internal/character/drratio/skill.go
@@ -3,8 +3,27 @@ package drratio
 import (
 	"github.com/simimpact/srsim/pkg/engine/info"
 	"github.com/simimpact/srsim/pkg/key"
+	"github.com/simimpact/srsim/pkg/model"
 )
 
 func (c *char) Skill(target key.TargetID, state info.ActionState) {
-	
+	debuffCount := c.engine.ModifierStatusCount(target, model.StatusType_STATUS_DEBUFF)
+	insertChance := 0.4 + (0.2 * float64(debuffCount))
+	if insertChance > 1.0 {
+		insertChance = 1.0
+	}
+
+	if c.engine.Rand().Float64() > insertChance {
+		// mark the enemy to be hit with FUA
+	}
+
+	// init a2 stacks
+
+	// do the damage
+
+	state.EndAttack()
+
+	// apply a4
+
+	// if
 }
