f0a155903d5bbb37a01966275b6c372c9960d98a
ducc <aaronhwang22@gmail.com>
2025-01-15 17:59:54 -0500

    basic atk implementation

--- internal/character/drratio/attack.go
+++ internal/character/drratio/attack.go
@@ -3,8 +3,26 @@ package drratio
 import (
 	"github.com/simimpact/srsim/pkg/engine/info"
 	"github.com/simimpact/srsim/pkg/key"
+	"github.com/simimpact/srsim/pkg/model"
+)
+
+const (
+	Normal = "drratio-normal"
 )
 
 func (c *char) Attack(target key.TargetID, state info.ActionState) {
+	c.engine.Attack(info.Attack{
+		Key:        Normal,
+		Source:     c.id,
+		Targets:    []key.TargetID{target},
+		AttackType: model.AttackType_NORMAL,
+		DamageType: model.DamageType_IMAGINARY,
+		BaseDamage: info.DamageMap{
+			model.DamageFormula_BY_ATK: Basic_ATK_0[c.info.AttackLevelIndex()],
+		},
+		StanceDamage: 30,
+		EnergyGain:   20,
+	})
 
+	state.EndAttack()
 }
